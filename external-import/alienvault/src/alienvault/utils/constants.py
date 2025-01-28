5db398c24d7d276af1aff31f44d1c45a0bae92d7
JagadeswarJk <thegod2040@gmail.com>
2025-01-28 17:12:05 +0530

    Update constants.py

--- external-import/alienvault/src/alienvault/utils/constants.py
+++ external-import/alienvault/src/alienvault/utils/constants.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""OpenCTI AlienVault utilities constants module."""
+"""threatlens AlienVault utilities constants module."""
 
 from stix2 import TLP_AMBER, TLP_GREEN, TLP_RED, TLP_WHITE  # type: ignore
 
@@ -13,13 +13,13 @@ TLP_MARKING_DEFINITION_MAPPING = {
 DEFAULT_TLP_MARKING_DEFINITION = TLP_WHITE
 
 
-X_OPENCTI_ALIASES = "x_opencti_aliases"
-X_OPENCTI_ORGANIZATION_TYPE = "x_opencti_organization_type"
-X_OPENCTI_RELIABILITY = "x_opencti_reliability"
-X_OPENCTI_LOCATION_TYPE = "x_opencti_location_type"
+X_THREATLENS_ALIASES = "x_threatlens_aliases"
+X_THREATLENS_ORGANIZATION_TYPE = "x_threatlens_organization_type"
+X_THREATLENS_RELIABILITY = "x_threatlens_reliability"
+X_THREATLENS_LOCATION_TYPE = "x_threatlens_location_type"
 X_MITRE_ID = "x_mitre_id"
-X_OPENCTI_REPORT_STATUS = "x_opencti_report_status"
-X_OPENCTI_SCORE = "x_opencti_score"
-X_OPENCTI_LABELS = "x_opencti_labels"
-X_OPENCTI_CREATED_BY_REF = "x_opencti_created_by_ref"
-X_OPENCTI_MAIN_OBSERVABLE_TYPE = "x_opencti_main_observable_type"
+X_THREATLENS_REPORT_STATUS = "x_threatlens_report_status"
+X_THREATLENS_SCORE = "x_threatlens_score"
+X_THREATLENS_LABELS = "x_threatlens_labels"
+X_THREATLENS_CREATED_BY_REF = "x_threatlens_created_by_ref"
+X_THREATLENS_MAIN_OBSERVABLE_TYPE = "x_threatlens_main_observable_type"
